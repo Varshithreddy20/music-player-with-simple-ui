a016b156fc255ed47cc52314481035416e91fd91
Varshithreddy20 <142265233+Varshithreddy20@users.noreply.github.com>
2023-08-15 14:35:09 +0530

    Add files via upload
    Assets here in this are just songs and images for the songs that's upto you can keep 5, 10 or more.

---
+++ data.js
@@ -0,0 +1,244 @@
+
+let songs = [
+    {
+        name: 'Treat You Better',
+        path: 'assets/music/Treat You Better.MPEG',
+        artist: 'Artist 1',
+        cover: 'assets/images/cover1.PNG'
+    },
+    {
+        name: 'Princess Doesnot Cry ',
+        path: 'assets/music/Princess Doesnot Cry.MPEG',
+        artist: 'Artist 2',
+        cover: 'assets/images/cover2.PNG'
+    },
+    {
+        name: 'Animals-Maroon 5',
+        path: 'assets/music/Animals-Maroon 5.MPEG',
+        artist: 'Artist 3',
+        cover: 'assets/images/cover3.PNG'
+    },
+    {
+        name: 'Sugar-Maroon 5',
+        path: 'assets/music/Sugar-Maroon 5.MPEG',
+        artist: 'artist 4',
+        cover: 'assets/images/cover4.PNG'
+    },
+    {
+        name: 'Sorry',
+        path: 'assets/music/Sorry.MPEG',
+        artist: 'Artist 5',
+        cover: 'assets/images/cover5.PNG'
+    },
+    {
+        name: 'Love Story',
+        path: 'assets/music/Love Story.MPEG',
+        artist: 'Artist 6',
+        cover: 'assets/images/cover6.PNG'
+    },
+    {
+        name: 'No Lie',
+        path: 'assets/music/No Lie.MPEG',
+        artist: 'Artist 7',
+        cover: 'assets/images/cover7.PNG'
+    },
+    {
+        name: 'The Middle',
+        path: 'assets/music/The Middle.MPEG',
+        artist: 'Artist 8',
+        cover: 'assets/images/cover8.PNG'
+    },
+    {
+        name: 'Counting Stars',
+        path: 'assets/music/Counting Stars.MPEG',
+        artist: 'Artist 9',
+        cover: 'assets/images/cover1.PNG'
+    },
+    {
+        name: 'What Makes You Beautiful',
+        path: 'assets/music/What Makes You Beautiful.MPEG',
+        artist: 'Artist 10',
+        cover: 'assets/images/cover16.jpg'
+    },
+    {
+        name: 'Baarish Ban Jana',
+        path: 'assets/music/Baarish Ban Jana.MPEG',
+        artist: 'Artist 11',
+        cover: 'assets/images/cover15.jfif'
+    },
+    {
+        name: 'Dil Diyan Gallam',
+        path: 'assets/music/Dil Diyan Gallam.MPEG',
+        artist: 'Artist 12',
+        cover: 'assets/images/cover16.jpg'
+    },
+    {
+        name: 'Raataan Lambiyan',
+        path: 'assets/music/Raataan Lambiyan.MPEG',
+        artist: 'Artist 13',
+        cover: 'assets/images/cover17.jpg'
+    },
+    {
+        name: 'The Breakup Song',
+        path: 'assets/music/The Breakup Song.MPEG',
+        artist: 'Artist 14',
+        cover: 'assets/images/cover19.jpg'
+    },
+    {
+        name: 'Tip Tip',
+        path: 'assets/music/Tip Tip.MPEG',
+        artist: 'Artist 15',
+        cover: 'assets/images/cover22.jpg'
+    },
+    {
+        name: 'Tu Aake Dekhle',
+        path: 'assets/music/Tu Aake Dekhle.MPEG',
+        artist: 'Artist 16',
+        cover: 'assets/images/cover9.PNG'
+    },
+    {
+        name: 'Humnava Mere',
+        path: 'assets/music/Humnava Mere.MPEG',
+        artist: 'Artist 17',
+        cover: 'assets/images/cover19.jpg'
+    },
+    {
+        name: 'Jugnu',
+        path: 'assets/music/Jugnu.MPEG',
+        artist: 'Artist 18',
+        cover: 'assets/images/cover18.jpg'
+    },
+    {
+        name: 'Samjhawan',
+        path: 'assets/music/Samjhawan.MPEG',
+        artist: 'Artist 19',
+        cover: 'assets/images/cover26.jpg'
+    },
+    {
+        name: 'Shayad',
+        path: 'assets/music/Shayad.MPEG',
+        artist: 'Artist 20',
+        cover: 'assets/images/cover30.jpg'
+    },
+    {
+        name: 'Srivalli',
+        path: 'assets/music/Srivalli.MPEG',
+        artist: 'Artist 31',
+        cover: 'assets/images/cover8.PNG'
+    },
+    {
+        name: 'Jai Balayya',
+        path: 'assets/music/Jai Balayya.MPEG',
+        artist: 'Artist 32',
+        cover: 'assets/images/cover15.jfif'
+    },
+    {
+        name: 'Beemla nayak',
+        path: 'assets/music/Beemla nayak.MPEG',
+        artist: 'Artist 33',
+        cover: 'assets/images/cover22.jpg'
+    },
+    {
+        name: 'Naatu Naatu',
+        path: 'assets/music/Naatu Naatu.MPEG',
+        artist: 'Artist 34',
+        cover: 'assets/images/cover28.jfif'
+    },
+    {
+        name: 'So so ga',
+        path: 'assets/music/So so ga.MPEG',
+        artist: 'Artist 35',
+        cover: 'assets/images/cover18.jpg'
+    },
+    {
+        name: 'Neelambari',
+        path: 'assets/music/Neelambari.MPEG',
+        artist: 'Artist 36',
+        cover: 'assets/images/cover4.PNG'
+    },
+    {
+        name: 'E rathale',
+        path: 'assets/music/E rathale.MPEG',
+        artist: 'Artist 37',
+        cover: 'assets/images/cover29.jpg'
+    },
+    {
+        name: 'Manasu mari',
+        path: 'assets/music/Manasu mari.MPEG',
+        artist: 'Artist 38',
+        cover: 'assets/images/cover5.PNG'
+    },
+    {
+        name: 'Saaranga dhariya',
+        path: 'assets/music/Saaranga dhariya.MPEG',
+        artist: 'Artist 39',
+        cover: 'assets/images/cover1.PNG'
+    },
+    {
+        name: 'Oka pranam',
+        path: 'assets/music/Oka pranam.MPEG',
+        artist: 'Artist 40',
+        cover: 'assets/images/cover24.jpg'
+    },
+    {
+        name: 'En Iniya Thanimaye',
+        path: 'assets/music/En Iniya Thanimaye.mp3',
+        artist: 'Artist 41',
+        cover: 'assets/images/cover8.PNG'
+    },
+    {
+        name: 'Piriyadha Enna',
+        path: 'assets/music/Piriyadha Enna.mp3',
+        artist: 'Artist 42',
+        cover: 'assets/images/cover20.jpg'
+    },
+    {
+        name: 'Vaseegara Zara Zara Mashup',
+        path: 'assets/music/Vaseegara Zara Zara Mashup.mp3',
+        artist: 'Artist 43',
+        cover: 'assets/images/cover5.PNG'
+    },
+    {
+        name: 'Happy Birthday',
+        path: 'assets/music/Happy Birthday.mp3',
+        artist: 'Artist 44',
+        cover: 'assets/images/cover25.jpg'
+    },
+    {
+        name: 'Kathari Poovazhagi',
+        path: 'assets/music/Kathari Poovazhagi.mp3',
+        artist: 'Artist 45',
+        cover: 'assets/images/cover30.jpg'
+    },
+    {
+        name: 'Chill Bro',
+        path: 'assets/music/Chill Bro.mp3',
+        artist: 'Artist 46',
+        cover: 'assets/images/cover15.jfif'
+    },
+    {
+        name: 'Yelo Pullelo',
+        path: 'assets/music/Yelo Pullelo.mp3',
+        artist: 'Artist 47',
+        cover: 'assets/images/cover1.PNG'
+    },
+    {
+        name: 'Kanave Nee Naan',
+        path: 'assets/music/Kanave Nee Naan.mp3',
+        artist: 'Artist 48',
+        cover: 'assets/images/cover23.jpeg'
+    },
+    {
+        name: 'Oru Chance Kudu',
+        path: 'assets/music/Oru Chance Kudu.mp3',
+        artist: 'Artist 49',
+        cover: 'assets/images/cover14.jpg'
+    },
+    {
+        name: 'Naan Un Joshua',
+        path: 'assets/music/Naan Un Joshua.mp3',
+        artist: 'Artist 50',
+        cover: 'assets/images/cover8.PNG'
+    },
+    
+]
